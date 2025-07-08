37aea4ef629a468e25bcc581d2924dcd12b18df0
Benzema242 <rubensscutt91@live.com>
2025-07-08 13:05:03 -0400

    Completing assignment 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
+namespace Exercise1
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is the Exercise1 Project.");
+            Console.WriteLine("This project is part of the C# Fundamentals course.");
+
+            Console.Write("What is your first name? ");
+            string firstname = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"Your name  is {lastname}, {firstname} {lastname}.");
+
+        }
     }
 }
