fae8e10af2683949620af4150e32637c0f79822a
Benzema242 <rubensscutt91@live.com>
2025-07-09 10:38:03 -0400

    Completing assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -10,13 +10,13 @@ namespace Exercise1
             Console.WriteLine("This project is part of the C# Fundamentals course.");
 
             Console.Write("What is your first name? ");
-            string firstname = Console.ReadLine();
+            string firstName = Console.ReadLine();
 
             Console.Write("What is your last name? ");
-            string lastname = Console.ReadLine();
+            string lastName = Console.ReadLine();
 
             Console.WriteLine();
-            Console.WriteLine($"Your name  is {lastname}, {firstname} {lastname}.");
+            Console.WriteLine($"Your name  is {lastName}, {firstName} {lastName}.");
 
         }
     }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // For the sake of time and already late on submission i won't me doing the stretch challenges here
+        //  but i will practice them later.
+
+    
+        //Console.Write("What is the magic number?  ");
+        Random randomGenerator  = new Random();
+        int magicNumber = randomGenerator.Next(1, 100); // Generate a random number between 1 and 100
+
+        int guess = -1;
+
+        while (guess != magicNumber)  // Loop until the guess is correct
+        {
+            Console.Write("Guess the magic number: ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed it!");
+            }
+
+        }
     }
 }
